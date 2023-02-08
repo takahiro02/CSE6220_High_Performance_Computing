5304525486232725eff261a86fbf32b07f4e92c7
takahiro02 <tfuruya6@gatech.edu>
2023-02-07 21:24:54 -0500

    corrected a syntac error in run.sh

--- assignment1/run.sh
+++ assignment1/run.sh
@@ -7,14 +7,15 @@ make int_calc			# make a binary again
 echo "["
 
 FIRST_ITER="yes"
-for proc_num in "1" "2" "4" "8" "16" "32" "64"
-  if [ "$FIRST_ITER" = "yes" ]; then
-    FIRST_ITER="no"
-  else
-    echo ","
-  fi
-  mpirun -np $proc_num ./int_calc 1000000
-  # change processor number for the same n = 10^6
+for proc_num in "1" "2" "4" "8" "16" "32" "64"; do
+    if [ "$FIRST_ITER" = "yes" ]
+    then
+	FIRST_ITER="no"
+    else
+	echo ","
+    fi
+    mpirun -np $proc_num ./int_calc 1000000
+    # change processor number for the same n = 10^6
 done
 
 echo "]"
